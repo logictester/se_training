56c560779eb9e7b6389fdeb1daee92475f23fc14
Alex Basin <logictester@me.com>
2025-02-13 17:06:12 -0500

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,14 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+
+	'sphinxcontrib.images',
+    'sphinx_copybutton',
+    'sphinx_rtd_theme',
+    'sphinx_tabs.tabs',
+    'sphinxcontrib.blockdiag'
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +31,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
